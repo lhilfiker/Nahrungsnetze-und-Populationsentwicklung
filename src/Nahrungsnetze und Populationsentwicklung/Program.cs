41356bcbefc6461d0fb2c95b2dba324f9728ca8d
Milan <m.jankovic.inf23@stud.bbbaden.ch>
2023-11-10 10:43:44 +0100

    created

---
+++ src/Nahrungsnetze und Populationsentwicklung/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Nahrungsnetze_und_Populationsentwicklung
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
