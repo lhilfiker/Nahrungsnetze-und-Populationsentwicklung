45ed8f6e659d65ec79051c87063852fb10f9032a
RebelCoderJames <142885453+RebelCoderJames@users.noreply.github.com>
2023-11-11 11:45:01 +0100

    Added SortByLayers. See code documentation on how to use

--- code-documentation.md
+++ code-documentation.md
@@ -21,3 +21,43 @@ For what:
     -  List float "EatsHowMany" 
     -  List bool "FoodOrEater" 
     - filepath of the file to save to
+
+
+
+## operation-helper.cs
+Class OperationHelper
+
+Functions:
+- Sort By Layer `OperationHelper.SortByLayer(all the lists listed down)`, this includes, in this order:
+    -  List string "Names"
+    -  List string "GetsEatenBy"
+    -  List string "Eats"
+    -  List float "Quantity" 
+    -  List float "EatsHowMany" 
+    -  List bool "FoodOrEater" 
+
+    **It will return the 2 Lists<int>**
+    - List 1: All Indexes of the input sorted by layer. e.g
+      | Indexes | Vaue |
+      |-----------------|-----------------|
+      | 0 | 15 |
+      | 1 | 12 |
+      | 2 | 5 |
+      | 3 | 8 |
+      | 4 | 11 |
+      | 5 | 7 |
+      | 6 | 3 |
+      | 7 | 1 |
+      | 8 | 2 |
+    - List 2: Where the next layer stops:
+      | Indexes | Vaue |
+      |-----------------|-----------------|
+      | 0 | 2 |
+      | 1 | 5 |
+      | 2 | 6 |
+      | 3 | 9 |
+
+    - The First Layer in this example goes to index 2 of the first List(index 0 of List2), The Second Layer goes to Index 5 of the first Layer, Layer 3 goes to Index 6 and Layer 4 to Index 9.
+
+
+
---
+++ src/operation-helper.cs
@@ -0,0 +1,75 @@
+
+
+using System.Runtime.CompilerServices;
+
+namespace Nahrungsnetze_und_Populationsentwicklung
+{
+    internal class OperationHelper
+    {
+
+
+        static (List<int>, List<int>)? SortByLayer(List<string> Names, List<string> GetsEatenBy, List<string> Eats,
+            List<float> Quantity, List<float> EatsHowMany, List<bool> FoodOrEater
+            )
+        {
+            int ItemsAdded = 0;
+            List<int> LatestFood = new();
+            List<int> AlreadyAdded = new();
+            List<int> TempLast = new();
+            List<int> LayersEnd = new();
+            while (ItemsAdded != Names.Count())
+            {
+                if (LatestFood.Count() == 0)
+                {
+                    int i = 0;
+                    foreach (var item in FoodOrEater)
+                    {
+                        if (item)
+                        {
+                            LatestFood.Add(i);
+                            AlreadyAdded.Add(i);
+                            ItemsAdded++;
+                        }
+
+                        i++;
+                    }
+                }
+                else
+                {
+                    int i = -1;
+                    TempLast.Clear();
+                    foreach (var item in Eats)
+                    {
+                        i++;
+                        if (FoodOrEater[i]) continue;
+                        if (AlreadyAdded.Contains(i)) continue;
+                        
+                        if(LatestFood.Contains(Names.IndexOf(item)))
+                        {
+                            TempLast.Add(i);
+                            AlreadyAdded.Add(i);
+                            ItemsAdded++;
+                        }
+                    }
+
+                    LatestFood.Clear();
+                    foreach (var item in TempLast)
+                    {
+                        LatestFood.Add(item);
+                    }
+
+                    TempLast.Clear();
+                }
+                LayersEnd.Add(AlreadyAdded.Last());
+            }
+            return (AlreadyAdded, LayersEnd);            
+        }
+        
+        static (List<string>, List<string>, List<string>, List<float>, List<float>, List<bool>)? Default(List<string> Names, List<string> GetsEatenBy, List<string> Eats,
+            List<float> Quantity, List<float> EatsHowMany, List<bool> FoodOrEater
+        )
+        {
+            return null;
+        }
+    }
+}
