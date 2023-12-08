47edfd38e5cdd7ea52c538f3e86921dc2e64f05d
RebelCoderJames <142885453+RebelCoderJames@users.noreply.github.com>
2023-12-08 08:23:33 +0100

    improve simulation

--- src/operation-helper.cs
+++ src/operation-helper.cs
@@ -140,87 +140,101 @@ namespace Nahrungsnetze_und_Populationsentwicklung
             }
         }
 
-        public static (List<float>, List<SpeciesData>) Simulate(List<string> names, List<string> eats, List<float> quantity,
-        List<float> eatsHowMany, List<float> deathsPerDay, List<float> replication, List<float> multiplier,
-        int days)
-    {
-        Dictionary<string, float> population = new Dictionary<string, float>();
-        List<SpeciesData> allSpeciesData = names.Select(name => new SpeciesData(name)).ToList();
-
-        for (int i = 0; i < names.Count; i++)
+        public static (List<float>, List<SpeciesData>) Simulate(List<string> names, List<string> eats,
+            List<float> quantity,
+            List<float> eatsHowMany, List<float> deathsPerDay, List<float> replication, List<float> multiplier,
+            int days)
         {
-            population[names[i]] = quantity[i];
-        }
-
-        Random random = new Random();
-
-        for (int day = 0; day < days; day++)
-        {
-            Dictionary<string, float> newPopulation = new Dictionary<string, float>(population);
-
+            Dictionary<string, float> population = new Dictionary<string, float>();
+            List<SpeciesData> allSpeciesData = names.Select(name => new SpeciesData(name)).ToList();
             for (int i = 0; i < names.Count; i++)
             {
-                string species = names[i];
-                float currentPopulation = population[species];
-                float birthRate = replication[i];
-                float deathRate = deathsPerDay[i];
-                float variationMultiplier = multiplier[i];
-
-                // Calculate probability for the event
-                float probability = CalculateProbability(variationMultiplier);
-
-                if (!string.IsNullOrEmpty(eats[i]) && population.ContainsKey(eats[i]))
-                {
-                    float availableFood = population[eats[i]];
-                    float foodConsumed = Math.Min(eatsHowMany[i], availableFood);
-                    if (random.NextDouble() < probability)
-                    {
-                        // Food consumption and reproduction
-                        float reproductionRate = (foodConsumed / eatsHowMany[i]) * birthRate;
-                        newPopulation[eats[i]] -= foodConsumed;
-                        newPopulation[species] += reproductionRate * currentPopulation;
-                    }
-                }
-                else
-                {
-                    if (random.NextDouble() < probability)
-                    {
-                        // Reproduction for producers
-                        newPopulation[species] += birthRate * currentPopulation;
-                    }
-                }
-
-                if (random.NextDouble() < probability)
-                {
-                    // Death
-                    newPopulation[species] -= deathRate * currentPopulation;
-                }
-
-                newPopulation[species] = Math.Max(0, newPopulation[species]);
+                population[names[i]] = quantity[i];
             }
 
-            // Update daily data for eac h species
-            for (int i = 0; i < names.Count; i++)
+            Random random = new Random();
+
+            (var layerIndexes, var layerBoundaries) = OperationHelper.SortByLayer(names, eats);
+
+            for (int day = 0; day < days; day++)
             {
-                allSpeciesData[i].DailyPopulations.Add(newPopulation[names[i]]);
+                Dictionary<string, float> newPopulation = new Dictionary<string, float>(population);
+
+                foreach (var idx in layerIndexes)
+                {
+                    string species = names[idx];
+                    float currentPopulation = population[species];
+
+                    if (currentPopulation <= 0)
+                    {
+                        allSpeciesData[idx].DailyPopulations.Add(0); 
+                        continue;
+                    }
+
+                    float birthRate = replication[idx];
+                    float deathRate = deathsPerDay[idx];
+                    float variationMultiplier = multiplier[idx];
+
+                    float totalFoodConsumed = 0f;
+                    if (!string.IsNullOrEmpty(eats[idx]))
+                    {
+                        string[] preySpecies = eats[idx].Split(',');
+                        foreach (string prey in preySpecies)
+                        {
+                            if (population.ContainsKey(prey.Trim()))
+                            {
+                                float availableFood = population[prey.Trim()];
+                                float foodConsumed = CalculateEventOutcome(Math.Min(eatsHowMany[idx], availableFood),
+                                    variationMultiplier, random);
+                                totalFoodConsumed += foodConsumed;
+                                newPopulation[prey.Trim()] = Math.Max(0, newPopulation[prey.Trim()] - foodConsumed);
+                            }
+                        }
+
+                        float survivalRate = Math.Min(1f, totalFoodConsumed / eatsHowMany[idx]);
+                        float survivingPopulation = currentPopulation * survivalRate;
+
+                        float reproductionOutcome = CalculateEventOutcome(birthRate * survivingPopulation,
+                            variationMultiplier, random);
+                        newPopulation[species] = survivingPopulation + reproductionOutcome;
+                    }
+                    else
+                    {
+                        float reproductionOutcome =
+                            CalculateEventOutcome(birthRate * currentPopulation, variationMultiplier, random);
+                        newPopulation[species] += reproductionOutcome;
+                    }
+
+                    float deathOutcome =
+                        CalculateEventOutcome(deathRate * newPopulation[species], variationMultiplier, random);
+                    newPopulation[species] = Math.Max(0, newPopulation[species] - deathOutcome);
+
+                    allSpeciesData[idx].DailyPopulations.Add(newPopulation[species]);
+                }
+
+                population = new Dictionary<string, float>(newPopulation);
             }
 
-            population = new Dictionary<string, float>(newPopulation);
+            List<float> finalPopulations = population.Values.ToList();
+            return (finalPopulations, allSpeciesData);
         }
-
-        List<float> finalPopulations = population.Values.ToList();
-        return (finalPopulations, allSpeciesData);
-    }
 
         private static float CalculateProbability(float multiplier)
         {
-            // Implementiere eine exponentielle Skalierung für die Wahrscheinlichkeit
-            // Multiplier-Werte reichen von 0 bis 100
-            if (multiplier >= 100) return 1.0f;
-            if (multiplier <= 0) return 1 / 1000000.0f; // Sehr geringe Wahrscheinlichkeit für 0
+            return Math.Clamp(multiplier, 0f, 1f);
+        }
 
-            // Exponentielle Skalierung
-            return (float)Math.Pow(10, multiplier / 100 - 1);
+        private static float CalculateEventOutcome(float eventAmount, float multiplier, Random random)
+        {
+            if (random.NextDouble() < multiplier)
+            {
+                return eventAmount;
+            }
+            else
+            {
+                double partialMultiplier = Math.Pow(random.NextDouble(), 2);
+                return (float)(eventAmount * partialMultiplier);
+            }
         }
     }
 }
